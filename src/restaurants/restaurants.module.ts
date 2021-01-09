2ce6ebdcb55f5c88f1b21e765fb61ca79bae40e3
chane81 <chane81@naver.com>
2021-01-09 20:31:54 +0900

    type orm 레포지토리 설정 추가

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantResolver } from './restaurants.resolver';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantResolver]
+  /** import repository */
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+
+  /** provider resolver, service */
+  providers: [RestaurantResolver, RestaurantService],
 })
-export class RestaurantsModule { }
+export class RestaurantsModule {}
