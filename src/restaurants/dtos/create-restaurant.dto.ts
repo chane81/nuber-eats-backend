6b56c7eef97a5560cc5381ef52359ae474f7cc5a
chane81 <chane81@naver.com>
2021-01-09 23:16:54 +0900

    update restaurant commit

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,5 +1,9 @@
 import { InputType, OmitType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entity';
 
+/**
+ * 데이터 INSERT 를 위한 DTO
+ * OmitType 은 상속받은 엔티티에서 특정 필드를 제외할 때 사용
+ * */
 @InputType()
 export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Injectable()
 export class RestaurantService {
@@ -22,5 +23,9 @@ export class RestaurantService {
     const newRestaurant = this.restaurant.create(createRestaurantDto);
 
     return this.restaurant.save(newRestaurant);
+  }
+
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    return this.restaurant.update(id, { ...data });
   }
 }
