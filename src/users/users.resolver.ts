b71c0eac2b6b505eae2d261290b2f09462032c9b
chane81 <chane81@naver.com>
2021-01-11 22:45:53 +0900

    createAccount() 추가

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,10 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import {
+  CreateAccountOutput,
+  CreateAccountInput,
+} from './dtos/create-account.dto';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -10,5 +14,30 @@ export class UsersResolver {
   @Query(() => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation(() => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return {
+        error: e,
+        ok: false,
+      };
+    }
   }
 }
