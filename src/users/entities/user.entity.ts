2c4881a4cf947e9ffa7bbae1b876d0296038a658
chane81 <chane81@naver.com>
2021-01-11 22:47:23 +0900

    Field 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,27 @@
+import { Field, InputType, registerEnumType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
 
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field(() => String)
   email: string;
 
   @Column()
+  @Field(() => String)
   password: string;
 
-  @Column()
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(() => UserRole)
   role: UserRole;
 }
