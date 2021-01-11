eee980fb51510bee5bb87382a571e847134e330c
chane81 <chane81@naver.com>
2021-01-11 22:46:50 +0900

    createAccount() 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { CreateAccountInput } from './dtos/create-account.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,26 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check new user
+    // create user & hash the password
+    try {
+      const exists = await this.users.findOne({ email });
+
+      if (exists) {
+        // make error
+        return 'There is a user with that email already';
+      }
+
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (e) {
+      // make error
+      return `Couldn't create account`;
+    }
+  }
 }
