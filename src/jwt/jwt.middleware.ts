f1c2cd0aaa5750f59b7272877855c06af6c81504
chane81 <chane81@naver.com>
2021-01-17 01:54:10 +0900

    jwt 를 가지고 req.user 에 user 데이터 넣기

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,19 +1,29 @@
-import { NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { JwtService } from './jwt.service';
+import { UsersService } from '../users/users.service';
 
+@Injectable()
 export class JwtMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.headers);
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt'];
+      const decoded = this.jwtService.verify(token.toString());
+
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.userService.findById(decoded['id']);
+          req['user'] = user;
+          console.log(user);
+        } catch (e) {}
+      }
+    }
+
     next();
   }
 }
-
-// 위의 클래스형 구현과 동일함 (단지 함수형으로 해봄)
-// export const jwtMiddleware = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-// ) => {
-//   console.log('=========================', req.headers);
-//   next();
-// };
