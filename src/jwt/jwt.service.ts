b2cc6f213eb7e397e206b31143aef5f7a3b02fa0
chane81 <chane81@naver.com>
2021-01-17 00:45:45 +0900

    ConfigService 제거

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 import * as jwt from 'jsonwebtoken';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtService {
