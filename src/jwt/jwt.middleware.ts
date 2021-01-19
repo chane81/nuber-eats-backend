3b7531be394dcc76081380c4d0bb0cdbc58db862
BM <chane81@naver.com>
2021-01-19 19:07:47 +0900

    console.log 제거

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -19,7 +19,6 @@ export class JwtMiddleware implements NestMiddleware {
         try {
           const user = await this.userService.findById(decoded['id']);
           req['user'] = user;
-          console.log(user);
         } catch (e) {}
       }
     }
