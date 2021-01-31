523498153d4ba5642316d2f99b0798eb505433e1
chane81 <chane81@naver.com>
2021-01-31 15:52:44 +0900

    Verification entity 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { Verification } from './entities/verification.entity';
 
 @Module({
   /** import repository */
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Verification])],
 
   /** provider resolver, service */
   providers: [UsersResolver, UsersService],
