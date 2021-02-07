510a715707bc018340593ba2e85493ea87142f4a
chane81 <chane81@naver.com>
2021-02-07 21:12:41 +0900

    test spec 작성 및 mock 작성

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,68 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+import { JwtService } from '../jwt/jwt.service';
+import { MailService } from '../mail/mail.service';
+
+/* ============================================= */
+/* mocking */
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
+/* mocking */
+/* ============================================= */
+
+describe('UserService', () => {
+  let service: UsersService;
+
+  beforeAll(async () => {
+    // module create
+    const modules = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+
+    // service 주입
+    service = modules.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+});
