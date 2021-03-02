a0526eb7778c775dc87d1f89b26c5bee1a1cc796
BM <chane81@naver.com>
2021-03-02 19:49:44 +0900

    createAccount 테스트코드 추가

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -21,13 +24,44 @@ describe('UserModule (e2e)', () => {
    * This usually means that there are asynchronous operations that weren't stopped in your tests. Consider running Jest with `--detectOpenHandles` to troubleshoot this issue.
    */
   afterAll(async () => {
+    // 테스트가 끝나고 database drop 수행
+    await getConnection().dropDatabase();
+
+    // warning 대응
     app.close();
   });
 
-  it.todo('me');
-  it.todo('userProfile');
+  describe('createAccount', () => {
+    const EMAIL = 'chane8323@naver.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "12345",
+                role: Client
+              }) {
+                error
+                ok
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+  });
+
   it.todo('createAccountInput');
+  it.todo('userProfile');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
