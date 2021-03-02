a4a453280f5ff4cde2648640d96eb94df12b4c16
BM <chane81@naver.com>
2021-03-02 19:29:19 +0900

    아래 warning 대응코드 추가 Jest did not exit one second after the test run has completed

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from '../src/app.module';
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -14,4 +14,20 @@ describe('UserModule (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
   });
+
+  /**
+   * 아래 warning 대응 코드
+   * Jest did not exit one second after the test run has completed.
+   * This usually means that there are asynchronous operations that weren't stopped in your tests. Consider running Jest with `--detectOpenHandles` to troubleshoot this issue.
+   */
+  afterAll(async () => {
+    app.close();
+  });
+
+  it.todo('me');
+  it.todo('userProfile');
+  it.todo('createAccountInput');
+  it.todo('login');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
 });
