87c7491fbfd95d1d705419a7a9880edd7a65b8a4
BM <chane81@naver.com>
2021-03-03 19:43:43 +0900

    login 테스트 추가

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,10 +4,23 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+// Jest did not exit one second after the test run has completed. 대응
+// nodemailer 로 메일 보내지 않게 mock 함
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockImplementation(() => ({
+    sendMail: jest.fn(() => true),
+  })),
+}));
+
 const GRAPHQL_ENDPOINT = '/graphql';
+const testUser = {
+  email: 'chane8323@naver.com',
+  password: '12345',
+};
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -32,7 +45,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'chane8323@naver.com';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -40,8 +52,8 @@ describe('UserModule (e2e)', () => {
           query: `
             mutation {
               createAccount(input: {
-                email: "${EMAIL}",
-                password: "12345",
+                email: "${testUser.email}",
+                password: "${testUser.password}",
                 role: Client
               }) {
                 error
@@ -56,11 +68,74 @@ describe('UserModule (e2e)', () => {
           expect(res.body.data.createAccount.error).toBe(null);
         });
     });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${testUser.email}",
+                password: "${testUser.password}",
+                role: Client
+              }) {
+                error
+                ok
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+
+          // toBe의 경우는 정확한 입력이 필요하다.
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+
+          // 정확한 에러 메시지를 알 수 없을 경우 toEqual로 expect.any(String) 를 이용하자.
+          // expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
   });
 
-  it.todo('createAccountInput');
-  it.todo('userProfile');
-  it.todo('login');
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              login(input: {
+                email: "${testUser.email}",
+                password: "${testUser.password}"
+              }) {
+                error
+                ok
+                token
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+
+          // 토큰 주입
+          jwtToken = login.token;
+        });
+    });
+  });
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
