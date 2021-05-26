7bf81797a7ad77ff3ef9d061bcc0cfa6f945a2ac
chane81 <chane81@naver.com>
2021-05-27 00:13:19 +0900

    restaurants output field 로 추가

--- src/restaurants/dtos/category.dto.ts
+++ src/restaurants/dtos/category.dto.ts
@@ -1,9 +1,7 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import {
-  PaginationInput,
-  PaginationOutput,
-} from 'src/common/dtos/pagination.dto';
+import { PaginationInput, PaginationOutput } from 'src/common/dtos/pagination.dto';
 import { Category } from '../entities/category.entity';
+import { Restaurant } from '../entities/restaurant.entity';
 
 @InputType()
 export class CategoryInput extends PaginationInput {
@@ -13,6 +11,9 @@ export class CategoryInput extends PaginationInput {
 
 @ObjectType()
 export class CategoryOutput extends PaginationOutput {
+  @Field(() => [Restaurant], { nullable: true })
+  restaurants?: Restaurant[];
+
   @Field(() => Category, { nullable: true })
   category?: Category;
 }
