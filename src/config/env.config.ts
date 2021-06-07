a1ee6f73cc9cce59241a17eef13dabccc15ec802
BM <chane81@naver.com>
2021-06-07 15:57:31 +0900

    AWS_S3_ACCKEY, AWS_S3_SECRET 추가 및 nullable 처리

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,13 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('env', () => ({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-  PRIATE_KEY: process.env.PRIATE_KEY,
-  NODEMAILER_USER: process.env.NODEMAILER_USER,
-  NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
-  NODEMAILER_FROM_EMAIL: process.env.NODEMAILER_FROM_EMAIL,
+  DB_HOST: process.env.DB_HOST || '',
+  DB_PORT: process.env.DB_PORT || '',
+  DB_USERNAME: process.env.DB_USERNAME || '',
+  DB_PASSWORD: process.env.DB_PASSWORD || '',
+  DB_NAME: process.env.DB_NAME || '',
+  PRIATE_KEY: process.env.PRIATE_KEY || '',
+  NODEMAILER_USER: process.env.NODEMAILER_USER || '',
+  NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD || '',
+  NODEMAILER_FROM_EMAIL: process.env.NODEMAILER_FROM_EMAIL || '',
+  AWS_S3_ACCKEY: process.env.AWS_S3_ACCKEY || '',
+  AWS_S3_SECRET: process.env.AWS_S3_SECRET || '',
 }));
