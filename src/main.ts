6d1c38d8840611b7b128cbba29c8bb4852bd1e2d
charles0628 <ch.lee@getcha.kr>
2021-09-04 18:09:49 +0900

    add config type

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigType } from '@nestjs/config';
+import envConfig from './config/env.config';
+
+// config type
+type configType = ConfigService<ConfigType<typeof envConfig>>;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService);
+  const config = app.get<configType>(ConfigService);
 
   // class-validator 사용을 위한 설정
   app.useGlobalPipes(new ValidationPipe());
